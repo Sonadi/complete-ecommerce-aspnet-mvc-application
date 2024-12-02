7171eda3316fec588cea10bcb78216e05a8edc3b
Sonadi <144614600+Sonadi@users.noreply.github.com>
2024-12-02 21:32:38 +0530

    Add project files.

---
+++ etickets1/Models/Actor.cs
@@ -0,0 +1,12 @@
+﻿namespace etickets1.Models
+{
+    public class Actor
+    {
+        [key]
+        public int ActorId { get; set; }
+        public String ProfilePictureURL { get; set; }
+        public String FullName { get; set; }
+        public String Bio { get; set; }
+
+    }
+}
