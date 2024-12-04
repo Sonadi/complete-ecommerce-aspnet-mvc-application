0b28e55a56411d36b02b6080d69517c661fa0cbd
Sonadi <144614600+Sonadi@users.noreply.github.com>
2024-12-04 21:11:05 +0530

    database context

---
+++ etickets1/Data/AppDbInitializer.cs
@@ -0,0 +1,39 @@
+﻿namespace etickets1.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder) {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope()) {
+
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //cinema
+                if (!context.Cinemas.Any()) { 
+
+                }
+                //actors
+                if (!context.Actors.Any())
+                {
+
+                }
+                //movies
+                if (!context.Movies.Any())
+                {
+
+                }
+                //producers
+                if (!context.Producers.Any())
+                {
+
+                }
+                //actors & movies
+                if (!context.Actors_Movies.Any())
+                {
+
+                }
+            }
+        }
+    }
+}
